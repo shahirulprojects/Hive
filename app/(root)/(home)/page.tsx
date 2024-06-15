04490064c48eab8b1dbe78d1f6db6fac0ad32320
shahirulprojects <shahiruliimalo@gmail.com>
2024-06-16 03:30:39 +0800

    feat:add homepage cards and modals

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
+  const now = new Date();
+  // specify which country format that we want to get and specify the maximum digit that we want to display (in this case we dont want to show seconds)
+  // The locale parameter in the toLocaleTimeString method specifies the format conventions for the output, such as the way time is displayed, the usage of AM/PM, and the ordering of time components.
+  // "en-US" specifies American English, where the time format is usually in 12-hour format with AM/PM.
+  // "en-GB" specifies British English, where the time format is typically in 24-hour format.
+  const time = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "Asia/Kuala_Lumpur",
+  });
+  // intl stands for international
+  // we use .format because initially it will return the date object but we dont want it in object, so we have to use .format to convert it into string
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    now
+  );
   return (
     // size-full will give 100% width and 100% height
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <div className="h-[400px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
+            Upcoming Meeting at: 12:30 PM
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
+      </div>
+      <div className="items-center justify-center mt-20 flex">
+        <MeetingTypeList />
+      </div>
     </section>
   );
 };
---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useState } from "react";
+import HomeCard from "./HomeCard";
+import { useRouter } from "next/navigation";
+import MeetingModal from "./MeetingModal";
+
+const MeetingTypeList = () => {
+  const router = useRouter();
+  // define the possible types of the states / what the state could possibly be
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >();
+
+  const createMeeting = () => {};
+  return (
+    // grid-cols- means that how many grid that will be shown in a row. If grid-cols-4, than it means that it will show 4 grid in a row (4 columns in a row)
+    <section className="grid grid-cols-1 gap-[100px] md:grid-cols-2 xl:grid-cols-4 ">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState("isInstantMeeting")}
+        className="bg-orange-1"
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
+        className="bg-blue-1"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Check out your recordings"
+        handleClick={() => router.push("/recordings")}
+        className="bg-purple-1"
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="Via invitation link"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
+        className="bg-yellow-1"
+      />
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"} // isOpen is true if the meetingState is isInstantMeeting
+        onClose={() => setMeetingState(undefined)} // set the meeting state to undefined to reset it
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
