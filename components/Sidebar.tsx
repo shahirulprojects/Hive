9ea9f61716f27b6c32fadff96d3231be578bac2e
shahirulprojects <shahiruliimalo@gmail.com>
2024-06-15 12:12:17 +0800

    feat:add basic sidebar and assets

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,46 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const pathname = usePathname();
+  return (
+    // sticky so that it stays there, left-0 so that it will be at the left side, top-0 so that it will be at top, h-screen for full width, max-sm meand that it will be applied to small screen and BELOW
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathname === link.route || pathname.startsWith(link.route);
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn(
+                "flex gap-4 items-center p-4 rounded-lg justify-start",
+                { "bg-blue-1": isActive }
+              )}
+            >
+              {/* cn is short for className and it allows us to add multiple and dynamic className */}
+              <Image
+                src={link.imgUrl}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Sidebar;
