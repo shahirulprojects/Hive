ac4894bede49c69702cd6cad83e9cdaa0e12e577
shahirulprojects <shahiruliimalo@gmail.com>
2024-06-19 14:31:06 +0800

    fix: fix live time and date display

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,21 +1,46 @@
+"use client";
+import { useState, useEffect } from "react";
 import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
-  const now = new Date();
   // specify which country format that we want to get and specify the maximum digit that we want to display (in this case we dont want to show seconds)
   // The locale parameter in the toLocaleTimeString method specifies the format conventions for the output, such as the way time is displayed, the usage of AM/PM, and the ordering of time components.
   // "en-US" specifies American English, where the time format is usually in 12-hour format with AM/PM.
   // "en-GB" specifies British English, where the time format is typically in 24-hour format.
-  const time = now.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    // timeZone: "Asia/Kuala_Lumpur",
-  });
+  // Declare state variables for time and date
+  const [time, setTime] = useState(
+    new Date().toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      // timeZone: "Asia/Kuala_Lumpur",
+    })
+  );
   // intl stands for international
   // we use .format because initially it will return the date object but we dont want it in object, so we have to use .format to convert it into string
-  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-    now
+
+  const [date, setDate] = useState(
+    new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(new Date())
   );
+
+  // useEffect to update the time every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      setTime(
+        now.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
+      setDate(
+        new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now)
+      );
+    }, 1000); // Update every second
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     // size-full will give 100% width and 100% height
     <section className="flex size-full flex-col gap-10 text-white">
